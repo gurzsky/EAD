8014cea076094343b5f3073244a7dbeec137fc6f
Carlos Eduardo Gurski <gurski.ce@gmail.com>
2022-01-01 11:51:26 -0300

    included FetchMode.SUBSELECT and custom query

--- course/src/main/java/com/ead/course/models/LessonModel.java
+++ course/src/main/java/com/ead/course/models/LessonModel.java
@@ -31,6 +31,6 @@ public class LessonModel implements Serializable {
     private LocalDateTime creationData;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @ManyToOne(optional = false)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
     private ModuleModel module;
 }
