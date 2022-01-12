33ec01688c06b29fdc83b8e5483bc30e5ecb52de
Carlos Eduardo Gurski <gurski.ce@gmail.com>
2022-01-12 11:12:20 -0300

    API Composition Pattern in Course microservices - Get All Users By Course

---
+++ course/src/main/java/com/ead/course/dtos/UserDto.java
@@ -0,0 +1,22 @@
+package com.ead.course.dtos;
+
+import com.ead.course.enums.UserStatus;
+import com.ead.course.enums.UserType;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDto {
+    private UUID userId;
+    private String userName;
+    private String email;
+    private String fullName;
+    private UserStatus userStatus;
+    private UserType userType;
+    private String phoneNumber;
+    private String cpf;
+    private String imageUrl;
+}
