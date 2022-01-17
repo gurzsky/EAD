5e80e8cc5584b84b874d4127cb9d72db27556923
Carlos Eduardo Gurski <gurski.ce@gmail.com>
2022-01-17 14:36:44 -0300

    delete course in CourseUserModel

--- authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.repositories.UserCourseRepository;
 import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +19,10 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
 
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserCourseRepository userCourseRepository;
 
     @Override
     public List<UserModel> findALL() {
@@ -31,6 +36,10 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void delete(UserModel userModel) {
+        List<UserCourseModel> userCourseModelList = userCourseRepository.findAllUsersCourseIntoUser(userModel.getUserId());
+        if (!userCourseModelList.isEmpty())
+            userCourseRepository.deleteAll(userCourseModelList);
+
         userRepository.delete(userModel);
     }
 
